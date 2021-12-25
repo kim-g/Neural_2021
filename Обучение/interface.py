050ab01f7c253e480b83f4faa1fd026b54164ddc
catgenadiy <elmoros312@gmail.com>
2021-12-25 15:57:55 +0500

    добавлен класс Sql_decode
    -посредник для хранения и передачи информации таблицы
    
    исправленна ошибка с кнопками возникающая из-за none в question

--- Обучение/interface.py
+++ Обучение/interface.py
@@ -2,12 +2,15 @@ from tkinter import *
 
 import tkinter as tk
 import numpy as np
+import sqlite3
 
 
 class GUI:
     def __init__(self):
         self.button_dict = dict()
         self.window = tk.Tk()
+        w,h = self.window.winfo_screenwidth(),self.window.winfo_screenheight()
+        self.window.geometry('%dx%d+0+0' % (w, h))
 
     def callback_function(self, x):
         self.OutVal = x
@@ -15,6 +18,8 @@ class GUI:
 
     def Show(self, Question, Answers):
         self.button_dict = dict()
+        if Question == None:
+            return
         lbl = Label(text='\n'+Question+'\n')
         lbl.pack()
         for index, dat in enumerate(Answers):
@@ -32,19 +37,43 @@ class GUI:
 
 class DATA: #собирает и выдаёт нужную информацию той или иной функции
    def __init__(self):
-       self.Queston_and_Button_answers = [["Вы уверены?", ["Да", "Не знаю", "Нет"]],
-                                          ['шашалык ?', ['да', 'нет', 'ответ']]]
+       self.Queston_and_Button_answers = []
+
        #переменная построенна по структуре :
        #[[question,[button_answers,button_answers]],[question,[button_answers,button_answers]]]
 
    def DATA_COLLECT(self):# возвращает значения введённые пользователем
        self.Outvalue = []
+
        for x in self.Queston_and_Button_answers:
            n = GUI()
-           self.Outvalue.append(n.Show(x[0], x[1]))
+           self.Outvalue.append(n.Show(x.Question, x.Button_answers))
        self.Outvalue = np.array(self.Outvalue)
        print(self.Outvalue)
+   def Load(self):
+       con = sqlite3.connect('answers_and_question (1-я копия).db3')
+       cur = con.cursor()
+       result_table = cur.execute('SELECT * FROM question')
+       for question in result_table.fetchall():
+           answers = cur.execute(f'SELECT answer FROM answers_for_buttons WHERE id_of_question ={question[0]}')
+           Quest = Sql_decode()
+           Quest.Question = question[1]
+           for answer in answers.fetchall() :
+               Quest.Button_answers.append(answer[0])
+           self.Queston_and_Button_answers.append(Quest)
+           print(Quest.Question,'\n',Quest.Button_answers)
+
+
+
+class Sql_decode:
+    def __init__(self):
+        self.Button_answers = []
+        self.Question = ''
+
+
+
 
 
 m = DATA()
-m.DATA_COLLECT()# вызываем
+m.Load()# вызываем
+m.DATA_COLLECT()
