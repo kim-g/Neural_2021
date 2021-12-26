1ae4bc80d57ce2c0f3a9e480da2be8b87f92150b
catgenadiy <elmoros312@gmail.com>
2021-12-26 18:07:15 +0500

    Добавлен lbl1 и lbl2 - lbl1 отвечает за кол-во ответов - lbl2 отвечает за кол-во оставшихся ответов

--- Обучение/interface.py
+++ Обучение/interface.py
@@ -9,19 +9,26 @@ class GUI:
     def __init__(self):
         self.button_dict = dict()
         self.window = tk.Tk()
-        w,h = self.window.winfo_screenwidth(),self.window.winfo_screenheight()
-        self.window.geometry('%dx%d+0+0' % (w, h))
+        self.w, self.h = self.window.winfo_screenwidth(),self.window.winfo_screenheight()
+        self.window.geometry('%dx%d+0+0' % (self.w, self.h))
+
 
     def callback_function(self, x):
         self.OutVal = x
         self.window.destroy()
 
-    def Show(self, Question, Answers):
+    def Show(self, Question, Answers, Amount_of_answers, num_of_question):
         self.button_dict = dict()
         if Question == None:
             return
+
         lbl = Label(text='\n'+Question+'\n')
+        lbl1 = Label(text=f'\n ответ номер : {num_of_question}/{Amount_of_answers[0][0]} \n', font='defult 19')
+        lbl2 = Label(text=f'\n осталось : {Amount_of_answers[0][0]-num_of_question}/{Amount_of_answers[0][0]} \n', font='defult 19')
         lbl.pack()
+        lbl1.place(x=(self.w-(self.w/4))-((self.w/4)/4)*3, y=self.h-(self.h/2))
+        lbl2.place(x=self.w/4, y=self.h-(self.h/2))
+
         for index, dat in enumerate(Answers):
             button = tk.Button(self.window, text=dat, command=lambda dat=dat,
             index=index: self.callback_function(index), width='20', height='2')
@@ -38,30 +45,33 @@ class GUI:
 class DATA: #собирает и выдаёт нужную информацию той или иной функции
    def __init__(self):
        self.Queston_and_Button_answers = []
-
+       self.con = sqlite3.connect('answers_and_question (1-я копия).db3')
+       self.cur = self.con.cursor()
        #переменная построенна по структуре :
        #[[question,[button_answers,button_answers]],[question,[button_answers,button_answers]]]
 
    def DATA_COLLECT(self):# возвращает значения введённые пользователем
        self.Outvalue = []
+       num_of_question = 0
 
        for x in self.Queston_and_Button_answers:
+           num_of_question += 1
            n = GUI()
-           self.Outvalue.append(n.Show(x.Question, x.Button_answers))
+           self.Outvalue.append(n.Show(x.Question, x.Button_answers, self.cur.execute('SELECT Count(question_text) FROM question').fetchall(), num_of_question))
        self.Outvalue = np.array(self.Outvalue)
        print(self.Outvalue)
+
    def Load(self):
-       con = sqlite3.connect('answers_and_question (1-я копия).db3')
-       cur = con.cursor()
-       result_table = cur.execute('SELECT * FROM question')
+       result_table = self.cur.execute('SELECT * FROM question')
        for question in result_table.fetchall():
-           answers = cur.execute(f'SELECT answer FROM answers_for_buttons WHERE id_of_question ={question[0]}')
+           answers = self.cur.execute(f'SELECT answer FROM answers_for_buttons WHERE id_of_question ={question[0]}')
            Quest = Sql_decode()
            Quest.Question = question[1]
+
            for answer in answers.fetchall() :
                Quest.Button_answers.append(answer[0])
+
            self.Queston_and_Button_answers.append(Quest)
-           print(Quest.Question,'\n',Quest.Button_answers)
 
 
 
