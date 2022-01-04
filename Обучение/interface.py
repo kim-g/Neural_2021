f7b07c8eb51c15dcb88d8eb208ac24a952d8a15e
catgenadiy <elmoros312@gmail.com>
2022-01-04 21:42:27 +0500

    изменения: -lbl3 - отвечает за credit -добавил игнорирование на вывод None из Show (теперь он не передаёт None в self.Outvalue)

--- Обучение/interface.py
+++ Обучение/interface.py
@@ -9,10 +9,10 @@ class GUI:
     def __init__(self):
         self.button_dict = dict()
         self.window = tk.Tk()
-        w, h = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
-        self.window.geometry('%dx%d+0+0' % (w, h))
+        self.w, self.h = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        self.window.geometry('%dx%d+0+0' % (self.w, self.h))
 
-    def callback_function(self, x):
+    def callback_function(self, x):#функция возврата информации в
         self.OutVal = x
         self.window.destroy()
 
@@ -20,21 +20,26 @@ class GUI:
         self.button_dict = dict()
         if Question == None:
             return
+        row, column =self.window.grid_size()
 
         lbl = Label(text='\n'+Question+'\n')
-        lbl1 = Label(self.window, text=f'\n Ответ номер : {num_of_question}/{Amount_of_answers[0][0]} \n', font='defult 19')
-        lbl2 = Label(self.window, text=f'\n Осталось : {Amount_of_answers[0][0]-num_of_question}/{Amount_of_answers[0][0]} \n', font='defult 19')
-        lbl.pack()
+        lbl1 = Label(text=f' Ответ номер : {num_of_question}/{Amount_of_answers[0][0]}'
+                     , font='defult 19')#виджет номера ответа
+        lbl2 = Label(text=f'Осталось : {Amount_of_answers[0][0]-num_of_question}/{Amount_of_answers[0][0]}'
+                     , font='defult 19')#виджет количества оставшихся отетов
+        lbl3 = Label(bg='Grey', fg='white', font='defult 10', text=f'\n   Credit\n\n\n\n\n', anchor='w')#виджет credit
 
+        lbl.pack()
         for index, dat in enumerate(Answers):
             button = tk.Button(self.window, text=dat, command=lambda dat=dat,
             index=index: self.callback_function(index), width='20', height='2')
             # кнопки номеруются сверху вниз
             button.pack()
             self.button_dict[dat] = button
+        lbl1.pack(side=RIGHT)
+        lbl2.pack(side=LEFT)
+        lbl3.place(relx=0, rely=0.84, width=self.w)
 
-        lbl1.pack(padx=2, pady=2, expand=1, fill=tk.BOTH, side=RIGHT)
-        lbl2.pack(padx=2, pady=2, expand=1, fill=tk.BOTH, side=LEFT)
         self.window.mainloop()
 
         return self.OutVal
@@ -57,11 +62,14 @@ class DATA: #собирает и выдаёт нужную информацию 
        for x in self.Queston_and_Button_answers:
            num_of_question += 1
            n = GUI()
-           self.Outvalue.append(n.Show(x.Question, x.Button_answers, self.cur.execute('SELECT Count(question_text) FROM question').fetchall(), num_of_question))
+           a = n.Show(x.Question, x.Button_answers, self.cur.execute('SELECT Count(question_text) FROM question').fetchall(), num_of_question)
+           if a == None:#игнорируем вывод None и незаписываем его в self.Outvalue
+               continue
+           self.Outvalue.append(a)
        self.Outvalue = np.array(self.Outvalue)
        print(self.Outvalue)
 
-   def Load(self):
+   def Load(self):#выгружаем и переводим данные из sql таблицы в понятные программе
        result_table = self.cur.execute('SELECT * FROM question')
        for question in result_table.fetchall():
            answers = self.cur.execute(f'SELECT answer FROM answers_for_buttons WHERE id_of_question ={question[0]}')
@@ -85,5 +93,5 @@ class Sql_decode:
 
 
 m = DATA()
-m.Load()# вызываем
+m.Load()
 m.DATA_COLLECT()
