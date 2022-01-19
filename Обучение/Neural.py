75230c25b4cebac786e2057f86a6077261de4db7
catgena <elmoros312@gmail.com>
2022-01-19 22:30:23 +0500

    добавил класс NN:  -загрузка  -сохранение

---
+++ Обучение/Neural.py
@@ -0,0 +1,11 @@
+import numpy as np
+from tensorflow import keras
+from keras import *
+
+class NN:
+    def create(self):
+        pass
+    def Load(self,model):#загрузка
+        model.load()
+    def Save(self,model):#сохранение
+        model.safe()
